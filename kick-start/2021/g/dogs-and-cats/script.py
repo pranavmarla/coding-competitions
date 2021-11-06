9b86bc579db1e20f11b4a400f65b08fa77eeef13
Pranav Marla <pranav.a.marla@gmail.com>
2021-11-06 19:13:07 -0400

    Fix bug introduced by previous attempt at improving efficiency; Improve efficiency; Clarify

--- kick-start/2021/g/dogs-and-cats/script.py
+++ kick-start/2021/g/dogs-and-cats/script.py
@@ -6,34 +6,40 @@ DOG = 'D'
 
 # Functions
 
-def process_test_case(test_case_num, num_dog_food, num_cat_food, num_cat_food_refills, animals_str, cat=CAT, dog=DOG, out_of_cat_food=False):
+def process_test_case(test_case_num, num_dog_food, num_cat_food, num_cat_food_refills, animals_str, cat=CAT, dog=DOG):
+
+    out_of_dog_food_at_beginning = False
+    out_of_cat_food_and_need_to_feed_cat = False
+
+    if num_dog_food == 0:
+        out_of_dog_food_at_beginning = True
 
     for animal in animals_str:
-            
-        if not out_of_cat_food:
+        
+        # Feeding has stopped: Fail if there are any dogs remaining in line
+        if out_of_dog_food_at_beginning or out_of_cat_food_and_need_to_feed_cat:
+            if animal == dog:
+                print(f'Case #{test_case_num}: NO')
+                return
+
+        else:
             if animal == cat:
                 if num_cat_food > 0:
                     num_cat_food -= 1
-                # Out of cat food -- cat cannot eat
+                # Cat needs to be fed, but we're out of cat food: Don't know if it's a fail at this point
                 else:
-                    out_of_cat_food = True
+                    out_of_cat_food_and_need_to_feed_cat = True
             # Animal is dog
             else:
                 if num_dog_food > 0:
                     num_dog_food -= 1
                     num_cat_food += num_cat_food_refills
-                # Out of dog food -- dog cannot eat
+                # Dog needs to be fed, but we're out of dog food: Fail
                 else:
                     print(f'Case #{test_case_num}: NO')
                     return
-
-        # Out of cat food -- need to see if there are any dogs remaining in line
-        else:
-            if animal == dog:
-                print(f'Case #{test_case_num}: NO')
-                return
-        
-    # Either we never ran out of food, or we ran out of cat food but there were no more dogs left in line (i.e. all dogs were still fed prior)
+    
+    # Either we never ran out of food, or we ran out of cat food but there were no more dogs left in line (i.e. all dogs had been fed prior)
     print(f'Case #{test_case_num}: YES')
 
 
@@ -42,12 +48,8 @@ def main():
     num_test_cases = int(input())
 
     for test_case in range(1, num_test_cases+1):
-        
         _, num_dog_food, num_cat_food, num_cat_food_refills = [int(int_str) for int_str in input().split()]
         animals_str = input()
-        
-        #! TODO: Edge cases when inputs are 0
-
         process_test_case(test_case, num_dog_food, num_cat_food, num_cat_food_refills, animals_str)
 
 
