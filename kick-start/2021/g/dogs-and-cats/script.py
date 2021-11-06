4c1c31932a34252bc1baaf7e7c65ceb12b8c52bb
Pranav Marla <pranav.a.marla@gmail.com>
2021-11-06 17:51:18 -0400

    Incorporate default test cases

--- kick-start/2021/g/dogs-and-cats/script.py
+++ kick-start/2021/g/dogs-and-cats/script.py
@@ -1,27 +1,22 @@
-import sys
+# Constants
 
 CAT = 'C'
 DOG = 'D'
-OUT_OF_CAT_FOOD = False
 
-num_test_cases = int(input())
 
-for i in range(1, num_test_cases+1):
-    
-    num_animals, num_dog_food, num_cat_food, num_cat_food_refills = [int(int_str) for int_str in input().split()]
-    animals_str = input()
-    
-    #! TODO: Edge cases when inputs are 0
+# Functions
+
+def process_test_case(test_case_num, num_dog_food, num_cat_food, num_cat_food_refills, animals_str, cat=CAT, dog=DOG, out_of_cat_food=False):
 
     for animal in animals_str:
-        
-        if not OUT_OF_CAT_FOOD:
-            if animal == CAT:
+            
+        if not out_of_cat_food:
+            if animal == cat:
                 if num_cat_food > 0:
                     num_cat_food -= 1
                 # Out of cat food -- cat cannot eat
                 else:
-                    OUT_OF_CAT_FOOD = True
+                    out_of_cat_food = True
             # Animal is dog
             else:
                 if num_dog_food > 0:
@@ -29,14 +24,32 @@ for i in range(1, num_test_cases+1):
                     num_cat_food += num_cat_food_refills
                 # Out of dog food -- dog cannot eat
                 else:
-                    print(f'Case #{i}: NO')
-                    sys.exit()
+                    print(f'Case #{test_case_num}: NO')
+                    return
 
         # Out of cat food -- need to see if there are any dogs remaining in line
         else:
-            if animal == DOG:
-                print(f'Case #{i}: NO')
-                sys.exit()
+            if animal == dog:
+                print(f'Case #{test_case_num}: NO')
+                return
         
     # Either we never ran out of food, or we ran out of cat food but there were no more dogs left in line (i.e. all dogs were still fed prior)
-    print(f'Case #{i}: YES')
+    print(f'Case #{test_case_num}: YES')
+
+
+def main():
+
+    num_test_cases = int(input())
+
+    for test_case in range(1, num_test_cases+1):
+        
+        _, num_dog_food, num_cat_food, num_cat_food_refills = [int(int_str) for int_str in input().split()]
+        animals_str = input()
+        
+        #! TODO: Edge cases when inputs are 0
+
+        process_test_case(test_case, num_dog_food, num_cat_food, num_cat_food_refills, animals_str)
+
+
+# Execution
+main()
