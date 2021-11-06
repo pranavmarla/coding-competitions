a7d3a35e1cb220f5b56c7023e10b35c8bfe995cf
Pranav Marla <pranav.a.marla@gmail.com>
2021-11-06 18:48:49 -0400

    Revert "Improve efficiency in edge cases; Make clearer"
    This reverts commit db7300d362a16007f61bbde7cd5193e1ee95f19a.

--- kick-start/2021/g/dogs-and-cats/script.py
+++ kick-start/2021/g/dogs-and-cats/script.py
@@ -6,34 +6,28 @@ DOG = 'D'
 
 # Functions
 
-def process_test_case(test_case_num, num_dog_food, num_cat_food, num_cat_food_refills, animals_str, cat=CAT, dog=DOG):
+def process_test_case(test_case_num, num_dog_food, num_cat_food, num_cat_food_refills, animals_str, cat=CAT, dog=DOG, out_of_cat_food=False):
 
-    out_of_food = False
-
-    # Edge cases
-    if (num_cat_food == 0) or (num_dog_food == 0):
-        out_of_food = True
-    
     for animal in animals_str:
             
-        if not out_of_food:
+        if not out_of_cat_food:
             if animal == cat:
                 if num_cat_food > 0:
                     num_cat_food -= 1
-                # Cat needs to be fed, but we're out of cat food: Not necessarily a fail
+                # Out of cat food -- cat cannot eat
                 else:
-                    out_of_food = True
+                    out_of_cat_food = True
             # Animal is dog
             else:
                 if num_dog_food > 0:
                     num_dog_food -= 1
                     num_cat_food += num_cat_food_refills
-                # Dog needs to be fed, but we're out of dog food: Automatic fail
+                # Out of dog food -- dog cannot eat
                 else:
                     print(f'Case #{test_case_num}: NO')
                     return
 
-        # Out of food: Automatic fail if there are any dogs remaining in line
+        # Out of cat food -- need to see if there are any dogs remaining in line
         else:
             if animal == dog:
                 print(f'Case #{test_case_num}: NO')
@@ -48,8 +42,12 @@ def main():
     num_test_cases = int(input())
 
     for test_case in range(1, num_test_cases+1):
+        
         _, num_dog_food, num_cat_food, num_cat_food_refills = [int(int_str) for int_str in input().split()]
         animals_str = input()
+        
+        #! TODO: Edge cases when inputs are 0
+
         process_test_case(test_case, num_dog_food, num_cat_food, num_cat_food_refills, animals_str)
 
 
