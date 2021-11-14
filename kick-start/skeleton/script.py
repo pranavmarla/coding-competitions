79019aad0dd9d6e7632e85661d54ccee0f0a23ce
Pranav Marla <pranav.a.marla@gmail.com>
2021-11-14 12:42:17 -0500

    Added more common inputs

--- kick-start/skeleton/script.py
+++ kick-start/skeleton/script.py
@@ -14,5 +14,7 @@ def process_test_case(test_case_num):
 num_test_cases = int(input())
 
 for test_case in range(1, num_test_cases+1):
+    _ = int(input())
     _ = [int(int_str) for int_str in input().split()]
+    _ = input()
     process_test_case(test_case)
